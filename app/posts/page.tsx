899693ac4a98649ce05e506e17599a72ce61fdb9
Edward Castaneda <aeghin7@gmail.com>
2024-05-30 10:39:03 -0500

    initial styling for blog

---
+++ app/posts/page.tsx
@@ -0,0 +1,13 @@
+
+
+const PostPage = () => {
+    return (
+        <main className="text-center pt-32 px-5">
+            <h1 className="text-4xl md:text-5xl font-bold mb-5">
+                posts here
+            </h1>
+        </main>
+    )
+};
+
+export default PostPage;
---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+    return (
+        <footer>
+            Header
+        </footer>
+    )
+};
+
+export default Footer;
